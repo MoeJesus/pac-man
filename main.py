8a0e37e2977603a2c57a026028c716814a636fc6
MoeJesus <daniel_reed34@aol.com>
2025-05-26 19:16:12 +0900

    First Commit

---
+++ main.py
@@ -0,0 +1,95 @@
+import pyxel
+import enum
+from player import Player, Direction
+
+
+# Game state labels
+class GameState(enum.Enum):
+    STARTING = 0
+    RUNNING = 1
+    DYING = 2
+    POWER_UP = 3
+    GAME_WIN = 4
+    GAME_OVER = 5
+
+
+class App:
+    def __init__(self):
+        pyxel.init(224, 288, display_scale = 3, title = "Pac-Man", fps = 60)
+        pyxel.load("assets/resources.pyxres")
+        self.current_game_state = GameState.STARTING
+        self.player = Player(pyxel.width / 2 - 8, pyxel.height / 2 + (7.5 * 8))
+        self.player_direction = self.player.direction
+        self.score = self.player.score
+        self.cookie_counter = self.player.cookie_counter
+        self.powered_up = self.player.powered
+        self.power_counter = self.player.power_count
+        self.frames = 0
+        self.start_stage()
+        pyxel.run(self.update, self.draw)
+
+    # Sets everything up to start the game
+    def start_stage(self):
+        self.init_tilemap()
+
+    # Sets all cookies to be collectable
+    def init_tilemap(self):
+        for y in range(36):
+            for x in range(28):
+                if pyxel.tilemaps[0].pget(x, y) == (1, 0):
+                    pyxel.tilemaps[0].pset(x, y, (2, 0))
+                elif pyxel.tilemaps[0].pget(x, y) == (1, 1):
+                    pyxel.tilemaps[0].pset(x, y, (2, 1))
+
+    # Sets that the starting time is appropriate
+    def starting_game(self):
+        self.frames += 1
+        if self.frames >= 240:
+            self.current_game_state = GameState.RUNNING
+            self.player.dx = -1
+            self.frames = 0
+
+    # Checks to see if all the cookies are eaten
+    def check_win(self):
+        if self.cookie_counter >= 244:
+            self.current_game_state = GameState.GAME_WIN
+
+    # Checks to see if the player is powered up
+    def check_powered_up(self):
+        if self.powered_up == True and self.power_counter > 0:
+            self.current_game_state = GameState.POWER_UP
+            self.power_counter -= 1
+        elif self.powered_up == True and self.power_counter <= 0:
+            self.powered_up = False
+            self.current_game_state = GameState.RUNNING
+
+    def update(self):
+        if self.current_game_state == GameState.STARTING:
+            self.starting_game()
+        if self.current_game_state == GameState.RUNNING or self.current_game_state == GameState.POWER_UP:
+            self.player.update_player()
+            self.score, self.cookie_counter, self.powered_up, self.power_counter = self.player.eat_cookies(self.player.x, self.player.y)
+            self.check_powered_up()
+            self.check_win()
+
+    def draw(self):
+        pyxel.cls(0)
+        self.player.draw_player(self.player_direction)
+        pyxel.bltm(0, 0, 0, 0, 0, pyxel.width, pyxel.height, 0)
+        pyxel.text(0, 0, str(self.score), 7)
+        # pyxel.text(100, 0, str(self.powered_up), 7)
+        pyxel.text(150, 0, str(self.current_game_state), 7)
+
+App()
+
+# https://s3.amazonaws.com/media-p.slid.es/uploads/565973/images/3452983/tiles.png
+
+#############################################
+#                To Do List                 #
+# Fix the issue with the power not ending   #
+# Makes lives counter and add it to the HUD #
+# Draw ghost sprites                        #
+# Create the AI for the ghosts              #
+#                                           #
+#                                           #
+#############################################
---
+++ player.py
@@ -0,0 +1,124 @@
+import pyxel
+import enum
+
+# Global variable for where the tiles for the wall start
+WALL_TILE_X = 8
+
+
+# Player direction labels
+class Direction(enum.Enum):
+    RIGHT = 0
+    DOWN = 1
+    LEFT = 2
+    UP = 3
+
+
+class Player:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.dx = 0
+        self.dy = 0
+        self.direction = Direction.LEFT
+        self.player_image = [32, 48, 16, 16]  # u, v, w, h
+        self.score = 0
+        self.cookie_counter = 0
+        self.powered = False
+        self.power_count = 0
+
+    def draw_player(self, direction):
+        sprite_x = self.player_image[0]
+        sprite_y = self.player_image[1]
+        width = self.player_image[2]
+        height = self.player_image[3]
+
+        if self.direction == Direction.UP:
+            sprite_x = pyxel.frame_count // 4 % 4 * 16
+            sprite_y = 32
+        elif self.direction == Direction.DOWN:
+            sprite_x = pyxel.frame_count // 4 % 4 * 16
+            sprite_y = 32
+            height = height * -1
+        elif self.direction == Direction.LEFT:
+            sprite_x = pyxel.frame_count // 4 % 4 * 16
+            sprite_y = 16
+            width = width * -1
+        elif self.direction == Direction.RIGHT:
+            sprite_x = pyxel.frame_count // 4 % 4 * 16
+            sprite_y = 16
+            
+        pyxel.blt(self.x, self.y, 0, sprite_x, sprite_y, width, height)
+
+    def update_player(self):
+        if pyxel.btn(pyxel.KEY_UP):
+            if not self.is_colliding(self.x, self.y - 2):
+                self.direction = Direction.UP
+                self.dx = 0
+                self.dy = -1
+        elif pyxel.btn(pyxel.KEY_DOWN):
+            if not self.is_colliding(self.x, self.y + 2):
+                self.direction = Direction.DOWN
+                self.dx = 0
+                self.dy = 1
+        elif pyxel.btn(pyxel.KEY_LEFT):
+            if not self.is_colliding(self.x - 3, self.y):
+                self.direction = Direction.LEFT
+                self.dx = -1
+                self.dy = 0
+        elif pyxel.btn(pyxel.KEY_RIGHT):
+            if not self.is_colliding(self.x + 3, self.y):
+                self.direction = Direction.RIGHT
+                self.dx = 1
+                self.dy = 0
+
+        self.x, self.y = self.push_back(self.x, self.y, self.dx, self.dy)
+
+        # If the player leaves the map on either end, they'll appear on the other side
+        if self.x <= -16:
+            self.x = pyxel.width
+        elif self.x >= pyxel.width:
+            self.x = -16
+
+    # Function for the player to eat cookies and add points
+    def eat_cookies(self, x, y):
+        x = x // 8 + .5
+        y = y // 8 + 1
+        if pyxel.tilemaps[0].pget(x, y) == (2, 0):
+            pyxel.tilemaps[0].pset(x, y, (0, 0))
+            self.score += 10
+            self.cookie_counter += 1
+        elif pyxel.tilemaps[0].pget(x, y) == (2, 1):
+            pyxel.tilemaps[0].pset(x, y, (0, 0))
+            self.score += 50
+            self.cookie_counter += 1
+            self.powered = True
+            self.power_count = 600
+            # eaten_ghosts = [False, False, False, False]
+        return self.score, self.cookie_counter, self.powered, self.power_count
+
+    # Helper functions to easily check for collisions to walls
+    def is_colliding(self, x, y):
+        x1 = (pyxel.floor(x) + 4) // 8
+        y1 = (pyxel.floor(y) + 4) // 8
+        x2 = (pyxel.ceil(x) + 11) // 8
+        y2 = (pyxel.ceil(y) + 11) // 8
+        for yi in range(y1, y2 + 1):
+            for xi in range(x1, x2 + 1):
+                if pyxel.tilemaps[0].pget(xi, yi)[0] >= WALL_TILE_X or pyxel.tilemaps[0].pget(xi, yi) == (0, 1):
+                    return True
+        return False
+
+    def push_back(self, x, y, dx, dy):
+        for _ in range(pyxel.ceil(abs(dy))):
+            step = max(-1, min(1, dy))
+            if self.is_colliding(x, y + step):
+                break
+            y += step
+            dy -= step
+        for _ in range(pyxel.ceil(abs(dx))):
+            step = max(-1, min(1, dx))
+            if self.is_colliding(x + step, y):
+                break
+            x += step
+            dx -= step
+        return x, y
